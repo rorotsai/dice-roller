495e0454c51871d6600a154f9da0c4b9e3d7f4b8
rorotsai <roro9075@gmail.com>
2022-11-16 15:16:51 +0800

    setting part done

--- dice prob calculator/main.c
+++ dice prob calculator/main.c
@@ -1,110 +1,93 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+int a1,a2,b1,b2,c1,c2,d1,d2,e1,e2,f1,f2;//first dice
+int g1,g2,h1,h2,i1,i2,j1,j2,k1,k2,l1,l2;//second dice
+int td1,tm11,tm12,tm13,tm14,tm15,tm16,tm17;
+int td2,tm21,tm22,tm23,tm24,tm25,tm26,tm27;
+
+fprobset(){
+    printf("Please using the fractional number\nEnter the molecular then the denominator\n");
+    printf("This is the first dice setting\n");
+    printf("Probability of 1 ?\n");
+    scanf("%d%d",&a1,&a2);
+    printf("probability of 1 is : %d / %d \n",a1,a2);
+    printf("Probability of 2 ?\n");
+    scanf("%d%d",&b1,&b2);
+    printf("probability of 2 is : %d / %d \n",b1,b2);
+    printf("Probability of 3 ?\n");
+    scanf("%d%d",&c1,&c2);
+    printf("probability of 3 is : %d / %d \n",c1,c2);
+    printf("Probability of 4 ?\n");
+    scanf("%d%d",&d1,&d2);
+    printf("probability of 4 is : %d / %d \n",d1,d2);
+    printf("Probability of 5 ?\n");
+    scanf("%d%d",&e1,&e2);
+    printf("probability of 5 is : %d / %d \n",e1,e2);
+    printf("Probability of 6 ?\n");
+    scanf("%d%d",&f1,&f2);
+    printf("probability of 6 is : %d / %d \n",f1,f2);
+    td1 = a2 * b2* c2 * d2 * e2 * f2;
+    tm11 = a1 * b2* c2 * d2 * e2 * f2;
+    tm12 = a2 * b1* c2 * d2 * e2 * f2;
+    tm13 = a2 * b2* c1 * d2 * e2 * f2;
+    tm14 = a2 * b2* c2 * d1 * e2 * f2;
+    tm15 = a2 * b2* c2 * d2 * e1 * f2;
+    tm16 = a2 * b2* c2 * d2 * e2 * f1;
+    tm17 = tm11+tm12+tm13+tm14+tm15+tm16;
+    if(tm17 == td1 ){
+        printf("Setting correctly\n");
+    }
+    if(tm17 != td1 ){
+        printf("Bad setting \n");
+        fprobset();
+    }
+}
+
+sprobset(){
+    printf("Please using the fractional number\nEnter the molecular then the denominator\n");
+    printf("This is the Second dice setting\n");
+    printf("Probability of 1 ?\n");
+    scanf("%d%d",&g1,&g2);
+    printf("probability of 1 is : %d / %d \n",g1,g2);
+    printf("Probability of 2 ?\n");
+    scanf("%d%d",&h1,&h2);
+    printf("probability of 2 is : %d / %d \n",h1,h2);
+    printf("Probability of 3 ?\n");
+    scanf("%d%d",&i1,&i2);
+    printf("probability of 3 is : %d / %d \n",i1,i2);
+    printf("Probability of 4 ?\n");
+    scanf("%d%d",&j1,&j2);
+    printf("probability of 4 is : %d / %d \n",j1,j2);
+    printf("Probability of 5 ?\n");
+    scanf("%d%d",&k1,&k2);
+    printf("probability of 5 is : %d / %d \n",k1,k2);
+    printf("Probability of 6 ?\n");
+    scanf("%d%d",&l1,&l2);
+    printf("probability of 6 is : %d / %d \n",l1,l2);
+    td2 = g2 * h2 * i2 * j2 * k2 * l2;
+    tm21 = g1 * h2 * i2 * j2 * k2 * l2;
+    tm22 = g2 * h1 * i2 * j2 * k2 * l2;
+    tm23 = g2 * h2 * i1 * j2 * k2 * l2;
+    tm24 = g2 * h2 * i2 * j1 * k2 * l2;
+    tm25 = g2 * h2 * i2 * j2 * k1 * l2;
+    tm26 = g2 * h2 * i2 * j2 * k2 * l1;
+    tm27 = tm21+tm22+tm23+tm24+tm25+tm26;
+    if(tm27 == td2 ){
+        printf("Setting correctly\n");
+    }
+    if(tm27 != td2 ){
+        printf("Bad setting \n");
+        sprobset();
+    }
+}
+
 int main()
 {
-    int l,m,n,o,p,q,r,s,t,u,v,y;
-    int all[1000];
-
-    printf("enter the ratio of the number 2\n");
-    scanf("%d",&l);
-    for(int x=0;x<l;x++){
-        printf("all[%d] = 2\n",x);
-        all[x] = 2 ;
-    }//done
-
-    printf("enter the ratio of the number 3\n");
-    scanf("%d",&m);
-    for(int x=0;x<m;x++){
-        printf("all[%d] = 3\n",x+l);
-        all[l+x] = 3 ;
-    }//done
-
-    printf("enter the ratio of the number 4\n");
-    scanf("%d",&n);
-    for(int x=0;x<n;x++){
-        printf("all[%d] = 3\n",x+m+l);
-        all[x+m+l] = 4 ;
-    }//done
-
-    printf("enter the ratio of the number 5\n");
-    scanf("%d",&o);
-    for(int x=0;x<o;x++){
-        printf("all[%d] = 5\n",n+x+m+l);
-        all[n+x+m+l] = 5 ;
-    }//done
-
-    printf("enter the ratio of the number 6\n");
-    scanf("%d",&p);
-    for(int x=0;x<p;x++){
-        printf("all[%d] = 6\n",o+n+x+m+l);
-        all[o+n+x+m+l] = 6 ;
-    }//done
-
-    printf("enter the ratio of the number 7\n");
-    scanf("%d",&q);
-    for(int x=0;x<q;x++){
-        printf("all[%d] = 7\n",p+o+n+x+m+l);
-        all[p+o+n+x+m+l] = 7 ;
-    }//done
-    printf("enter the ratio of the number 8\n");
-    scanf("%d",&r);
-    for(int x=0;x<r;x++){
-        printf("all[%d] = 8\n",q+p+o+n+x+m+l);
-        all[q+p+o+n+x+m+l] = 8 ;
-    }//done
-    printf("enter the ratio of the number 9\n");
-    scanf("%d",&s);
-    for(int x=0;x<s;x++){
-        printf("all[%d] = 9\n",r+q+p+o+n+x+m+l);
-        all[r+q+p+o+n+x+m+l] = 9 ;
-    }//done
-
-    printf("enter the ratio of the number 10\n");
-    scanf("%d",&t);
-    for(int x=0;x<t;x++){
-        printf("all[%d] = 10\n",s+r+q+p+o+n+x+m+l);
-        all[s+r+q+p+o+n+x+m+l] = 10 ;
-    }//done
-
-    printf("enter the ratio of the number 11\n");
-    scanf("%d",&u);
-    for(int x=0;x<u;x++){
-        printf("all[%d] = 11\n",t+s+r+q+p+o+n+x+m+l);
-        all[t+s+r+q+p+o+n+x+m+l] = 11 ;
-    }//done
-
-    printf("enter the ratio of the number 12\n");
-    scanf("%d",&v);
-    for(int x=0;x<v;x++){
-        printf("all[%d] = 12\n",u+t+s+r+q+p+o+n+x+m+l);
-        all[u+t+s+r+q+p+o+n+x+m+l] = 12 ;
-    }
-    for(int x = 0; x<v+u+t+s+r+q+p+o+n+m+l;x++){
-        printf("%d\n",all[x]);
-    }
-    while(1){
-        printf("Enter 1 to roll the dices\n");
-        scanf("%d",&y);
-        for(int c= 0;c<500000000;c++) c+=1;
-        if(y == 1){
-            srand( time(NULL) );
-            int z = rand();
-            int a;
-            a = z%(v+u+t+s+r+q+p+o+n+m+l);
-            printf("%d\n", all[a]);
-
-        }
-        else break;
-    }
-    return 0;
+    fprobset();
+    sprobset();
+    double x= 0;
+    x = (float)/z
+    printf("The probability of 2 is %.3f %", x);
 }
