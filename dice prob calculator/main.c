d2ad6473621e010b0e4ffe7a93d5a6697de7c354
rorotsai <roro9075@gmail.com>
2022-11-21 20:02:42 +0800

    first edition

--- dice prob calculator/main.c
+++ dice prob calculator/main.c
@@ -6,6 +6,8 @@ int a1,a2,b1,b2,c1,c2,d1,d2,e1,e2,f1,f2;//first dice
 int g1,g2,h1,h2,i1,i2,j1,j2,k1,k2,l1,l2;//second dice
 int td1,tm11,tm12,tm13,tm14,tm15,tm16,tm17;
 int td2,tm21,tm22,tm23,tm24,tm25,tm26,tm27;
+double ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9,ans10,ans11,ans12;
+//x = (float)ans1/z
 
 fprobset(){
     printf("Please using the fractional number\nEnter the molecular then the denominator\n");
@@ -87,7 +89,38 @@ int main()
 {
     fprobset();
     sprobset();
-    double x= 0;
-    x = (float)/z
-    printf("The probability of 2 is %.3f %", x);
+    ans2 = ((float)tm11/td1)*((float)tm21/td2);
+    ans2 = ans2*100;
+    ans3 = ((float)tm11/td1)*((float)tm22/td2)+((float)tm12/td1)*((float)tm21/td2);
+    ans3 = ans3*100;
+    ans4 = ((float)tm11/td1)*((float)tm23/td2)+((float)tm12/td1)*((float)tm22/td2)+((float)tm13/td1)*((float)tm21/td2);
+    ans4 = ans4*100;
+    ans5 = ((float)tm11/td1)*((float)tm24/td2)+((float)tm12/td1)*((float)tm23/td2)+((float)tm13/td1)*((float)tm22/td2)+((float)tm14/td1)*((float)tm21/td2);
+    ans5 = ans5*100;
+    ans6 = ((float)tm11/td1)*((float)tm25/td2)+((float)tm12/td1)*((float)tm24/td2)+((float)tm13/td1)*((float)tm23/td2)+((float)tm14/td1)*((float)tm22/td2)+((float)tm15/td1)*((float)tm21/td2);
+    ans6 = ans6*100;
+    ans7 = ((float)tm11/td1)*((float)tm26/td2)+((float)tm12/td1)*((float)tm25/td2)+((float)tm13/td1)*((float)tm24/td2)+((float)tm14/td1)*((float)tm23/td2)+((float)tm15/td1)*((float)tm22/td2)+((float)tm16/td1)*((float)tm21/td2);
+    ans7 = ans7*100;
+    ans8 = ((float)tm12/td1)*((float)tm26/td2)+((float)tm13/td1)*((float)tm25/td2)+((float)tm14/td1)*((float)tm24/td2)+((float)tm15/td1)*((float)tm23/td2)+((float)tm16/td1)*((float)tm22/td2);
+    ans8 = ans8*100;
+    ans9 = ((float)tm13/td1)*((float)tm26/td2)+((float)tm14/td1)*((float)tm25/td2)+((float)tm15/td1)*((float)tm24/td2)+((float)tm16/td1)*((float)tm23/td2);
+    ans9 = ans9*100;
+    ans10 = ((float)tm14/td1)*((float)tm26/td2)+((float)tm15/td1)*((float)tm25/td2)+((float)tm16/td1)*((float)tm24/td2);
+    ans10 = ans10*100;
+    ans11 = ((float)tm15/td1)*((float)tm26/td2)+((float)tm16/td1)*((float)tm25/td2);
+    ans11 = ans11*100;
+    ans12 = ((float)tm16/td1)*((float)tm26/td2);
+    ans12 = ans12*100;
+    printf("The probability of 2 is %.3f %\n", ans2);
+    printf("The probability of 3 is %.3f %\n", ans3);
+    printf("The probability of 4 is %.3f %\n", ans4);
+    printf("The probability of 5 is %.3f %\n", ans5);
+    printf("The probability of 6 is %.3f %\n", ans6);
+    printf("The probability of 7 is %.3f %\n", ans7);
+    printf("The probability of 8 is %.3f %\n", ans8);
+    printf("The probability of 9 is %.3f %\n", ans9);
+    printf("The probability of 10 is %.3f %\n", ans10);
+    printf("The probability of 11 is %.3f %\n", ans11);
+    printf("The probability of 12 is %.3f %\n", ans12);
+
 }
